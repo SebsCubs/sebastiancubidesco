67cbebca4751e34e77c1d51c066396d0e7117883
SebsCubs <ascespedesc@unal.edu.co>
2024-10-11 14:54:16 +0200

    updating content urls

--- contentIndex.js
+++ contentIndex.js
@@ -1,9 +1,9 @@
 const blogPosts = [
-    { id: 'post1', title: 'My First Post', url: 'blog/post1.md' },
-    { id: 'post2', title: 'My Second Post', url: 'blog/post2.md' }
+    { id: 'post1', title: 'A whitepaper on my website', url: '/blog/post1.md' },
+    { id: 'post2', title: 'My Second Post', url: '/blog/post2.md' }
 ];
 
 const projects = [
-    { id: 'project1', title: 'Awesome Project', url: 'projects/project1.html' },
-    { id: 'project2', title: 'Another Project', url: 'projects/project2.html' }
+    { id: 'project1', title: 'Awesome Project', url: '/projects/project1.html' },
+    { id: 'project2', title: 'Another Project', url: '/projects/project2.html' }
 ];
